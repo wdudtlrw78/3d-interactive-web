c2e9fc5bc90e4e3762419622fc5482cb787bc5ae
wdudtlrw78 <wdudtlrw78@gmail.com>
2022-09-12 19:15:12 +0900

    feat: basic scene - mesh

--- src/basic/src/main.js
+++ src/basic/src/main.js
@@ -6,13 +6,17 @@ import * as THREE from 'three';
 // document.body.appendChild(renderer.domElement);
 
 const canvas = document.querySelector('#three-canvas');
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    // TODO: antialias -> Mesh의 계단현상(안티에이징)이 생기는데 true 해주면 부드럽게 해준다. 대신에 성능에서는 약간 떨어질 수 있다.
+    antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Scene
+// TODO: Scene
 const scene = new THREE.Scene();
 
-// Camera
+// TODO: Camera
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -21,5 +25,20 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 // TODO: position 속성으로 위치를 셋팅할 수 있다. 만약 셋팅을 안해주면 기본 값으로 0, 0, 0이다.
+camera.position.x = 1;
+camera.position.y = 2;
 camera.position.z = 5;
 scene.add(camera);
+
+// TODO Mesh : geometry + material
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    // color: '0xff0000'
+    // color: '#ff0000'
+    color: 'red'
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// TODO: 그리기
+renderer.render(scene, camera);
