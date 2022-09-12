0d90d9288ebeab89807ada68bb01342b3ba75546
wdudtlrw78 <wdudtlrw78@gmail.com>
2022-09-12 18:10:41 +0900

    feat: webpack config

---
+++ src/03_webpack/src/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import {message} from "./message.js";
+
+message('hello three js');
---
+++ src/03_webpack/src/message.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export function message(msg) {
+    const elem = document.createElement('p');
+    elem.innerHTML = msg;
+    document.body.append(elem);
+}
